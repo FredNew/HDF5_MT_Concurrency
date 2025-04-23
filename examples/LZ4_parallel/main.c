187666104c62821f3b7449868608cb789ec50912
Frederick Neu <frederick.neu@studium.uni-hamburg.de>
2025-04-23 16:13:00 +0200

    POSIX run time

--- examples/LZ4_parallel/main.c
+++ examples/LZ4_parallel/main.c
@@ -2,27 +2,29 @@
 #include "dataset.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define H5Z_FILTER_LZ4 32004
 
 int main(int argc, char* argv[]){
+
+	struct timespec start, end;
+
     int rank = 2;
 
     hsize_t dims[rank];
     hsize_t chunk_dims[rank];
 
     dims[0] = 4*16*1024;
-    dims[1] = 4*1024;
+    dims[1] = 4*3*1024;
 
-    chunk_dims[0] = 4*1024; //down
+    chunk_dims[0] = 1024; //down
     chunk_dims[1] = 1024; //right
 
     hsize_t dset_size = dims[0] * dims[1];
     hsize_t chunk_size = chunk_dims[0] * chunk_dims[1];
 
     int* dset = create_int_ds(dset_size);
-
-    printf("Dataset created.\n");
 
     hid_t file_id, fspace_id, dset_id, dcpl_id;
 
@@ -34,24 +36,26 @@ int main(int argc, char* argv[]){
     const unsigned int cd_values[1] = {8*1024};
 
     if(H5Zfilter_avail(H5Z_FILTER_LZ4)){
-        printf("Filter %d available. Being applied. Block-Setting: %d.\n", H5Z_FILTER_LZ4, cd_values[0]);
         H5Pset_filter(dcpl_id, H5Z_FILTER_LZ4, H5Z_FLAG_MANDATORY, (size_t) 1, cd_values);
     }else{
         printf("Filter %d not found. Not applied.\n", H5Z_FILTER_LZ4);
     }
 
     fspace_id = H5Screate_simple(rank, dims, NULL);
-
     dset_id = H5Dcreate(file_id, "LZ4 parallel", H5T_NATIVE_INT, fspace_id, H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
 
-    printf("Beginning write...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start);
     H5Dwrite_filter_parallel(dset_id, H5S_ALL, dset, 4);
-    printf("Write completed.\n");
+    clock_gettime(CLOCK_MONOTONIC, &end);
 
     free(dset);
 
     H5Dclose(dset_id);
     H5Sclose(fspace_id);
     H5Fclose(file_id);
+
+    double elapsed = (end.tv_sec - start.tv_sec);
+    elapsed += (end.tv_nsec - start.tv_nsec)/1000000000.0;
+    printf("; %lf\n", elapsed); //Wall clock
     return 0;
 }
--- examples/LZ4_parallel/norm.c
+++ examples/LZ4_parallel/norm.c
@@ -1,28 +1,29 @@
 #include "hdf5.h"
 #include "dataset.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define H5Z_FILTER_LZ4 32004
 
 int main(int argc, char* argv[]){
+    struct timespec start, end;
+
     int rank = 2;
 
     hsize_t dims[rank];
     hsize_t chunk_dims[rank];
 
     dims[0] = 4*16*1024;
-    dims[1] = 4*1024;
+    dims[1] = 4*3*1024;
 
-    chunk_dims[0] = 4*1024; //down
+    chunk_dims[0] = 1024; //down
     chunk_dims[1] = 1024; //right
 
     hsize_t dset_size = dims[0] * dims[1];
     hsize_t chunk_size = chunk_dims[0] * chunk_dims[1];
 
     int* dset = create_int_ds(dset_size);
-
-    printf("Dataset created.\n");
 
     hid_t file_id, fspace_id, dset_id, dcpl_id;
 
@@ -34,7 +35,6 @@ int main(int argc, char* argv[]){
     const unsigned int cd_values[1] = {8*1024};
 
     if(H5Zfilter_avail(H5Z_FILTER_LZ4)){
-        printf("Filter %d available. Being applied. Block-Setting: %d.\n", H5Z_FILTER_LZ4, cd_values[0]);
         H5Pset_filter(dcpl_id, H5Z_FILTER_LZ4, H5Z_FLAG_MANDATORY, (size_t) 1, cd_values);
     }else{
         printf("Filter %d not found. Not applied.\n", H5Z_FILTER_LZ4);
@@ -44,12 +44,18 @@ int main(int argc, char* argv[]){
 
     dset_id = H5Dcreate(file_id, "LZ4 parallel", H5T_NATIVE_INT, fspace_id, H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
 
-    printf("Beginning write...\n");
+    clock_gettime(CLOCK_MONOTONIC, &start);
     H5Dwrite(dset_id, H5T_NATIVE_INT, H5S_ALL, fspace_id, H5P_DEFAULT, dset);
-    printf("Write completed.\n");
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    free(dset);
 
     H5Dclose(dset_id);
     H5Sclose(fspace_id);
     H5Fclose(file_id);
+
+    double elapsed = (end.tv_sec - start.tv_sec);
+    elapsed += (end.tv_nsec - start.tv_nsec)/1000000000.0;
+    printf("; %lf\n", elapsed); //Wall clock
     return 0;
 }
